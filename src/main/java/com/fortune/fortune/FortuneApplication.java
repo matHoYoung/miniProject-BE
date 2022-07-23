3e4ea660c8b8937571a83b4bf9fc2c7949fec531
github username <hy9825@naver.com>
2022-07-23 17:49:45 +0900

    운세 프로젝트 로그인, 회원가입, 로그아웃 완료

--- src/main/java/com/fortune/fortune/FortuneApplication.java
+++ src/main/java/com/fortune/fortune/FortuneApplication.java
@@ -2,7 +2,9 @@ package com.fortune.fortune;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class FortuneApplication {
 
@@ -11,3 +13,4 @@ public class FortuneApplication {
     }
 
 }
+
