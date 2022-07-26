3f1241eb053cada511bf8003c16868ce6602d734
github username <hy9825@naver.com>
2022-07-26 09:52:01 +0900

    22.07.26

--- src/main/java/com/fortune/fortune/service/UserService.java
+++ src/main/java/com/fortune/fortune/service/UserService.java
@@ -6,12 +6,10 @@ import com.fortune.fortune.dto.SignupRequestDto;
 import com.fortune.fortune.model.User;
 import com.fortune.fortune.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-import javax.jws.soap.SOAPBinding;
 import java.util.Arrays;
 import java.util.Optional;
 
